6a783753810a9ae1d4cf41eb940bb6c3ecce0815
Kasador <huntershaw23@gmail.com>
2019-06-11 18:55:06 -0500

    ADDED: styled all the buttons and fixed display screen. Will work on functionality.

--- src/Buttons/Buttons.js
+++ src/Buttons/Buttons.js
@@ -11,29 +11,29 @@ const Buttons = () => {
             </div>
             {/* mid 1 */}
             <div className="mid1 buttonsFlex">
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
+                <button className="numbers">7</button>
+                <button className="numbers">8</button>
+                <button className="numbers">9</button>
                 <button className="calculations">&#215;</button>
             </div>
             {/* mid 2 */}
             <div className="mid2 buttonsFlex">
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
+                <button className="numbers">4</button>
+                <button className="numbers">5</button>
+                <button className="numbers">6</button>
                 <button className="calculations">&#x2212;</button>
             </div>
             {/* mid 3 */}
             <div className="mid3 buttonsFlex">
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
+                <button className="numbers">1</button>
+                <button className="numbers">2</button>
+                <button className="numbers">3</button>
                 <button className="calculations">&#x2b;</button>
             </div>
             {/* bottom */}
             <div className="bottom buttonsFlex">
-                <button>0</button>
-                <button>.</button>
+                <button className="numbers zero">0</button>
+                <button className="numbers">.</button>
                 <button className="calculations">&#x3d;</button>
             </div>
         </div>

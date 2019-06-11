20caa432cf5c27be295a51b352739d385f62b154
Kasador <huntershaw23@gmail.com>
2019-06-11 18:59:59 -0500

    ADDED: fixed some styles and added simple title as well.

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,8 +4,16 @@ import Buttons from './Buttons/Buttons';
 
 class Calculator extends Component {
     render() {
+        const styles = {
+            borderBottom: '2px solid gray',
+            color: 'gray',
+            padding: '10px',
+            marginBottom: '50px'
+        }
+
         return (
             <div className="calculatorMain">
+                <h1 style={styles}>My Simple Calculator Made With React</h1>
                 <span className="display">0</span>
                 <Buttons />
             </div>

b3f0e5af89b9079009d321982145be48718ede50
Kasador <huntershaw23@gmail.com>
2019-06-13 22:54:16 -0500

    ADDED: now able to concat numbers together and output to display, still working on addition button.

--- src/Buttons/Buttons.js
+++ src/Buttons/Buttons.js
@@ -33,30 +33,30 @@ class Buttons extends Component {
                 </div>
                 {/* mid 1 */}
                 <div className="mid1 buttonsFlex">
-                    <button className="numbers" value="7">7</button>
-                    <button className="numbers" value="8">8</button>
-                    <button className="numbers" value="9">9</button>
+                    <button className="numbers" value="7" onClick={this.props.seven}>7</button>
+                    <button className="numbers" value="8" onClick={this.props.eight}>8</button>
+                    <button className="numbers" value="9" onClick={this.props.nine}>9</button>
                     <button className="calculations">&#215;</button>
                 </div>
                 {/* mid 2 */}
                 <div className="mid2 buttonsFlex">
-                    <button className="numbers" value="4">4</button>
-                    <button className="numbers" value="5">5</button>
-                    <button className="numbers" value="6">6</button>
+                    <button className="numbers" value="4" onClick={this.props.four}>4</button>
+                    <button className="numbers" value="5" onClick={this.props.five}>5</button>
+                    <button className="numbers" value="6" onClick={this.props.six}>6</button>
                     <button className="calculations">&#x2212;</button>
                 </div>
                 {/* mid 3 */}
                 <div className="mid3 buttonsFlex">
-                    <button className="numbers" value="1" onClick={() => this.value(this.value)}>1</button>
-                    <button className="numbers" value="2">2</button>
-                    <button className="numbers" value="3">3</button>
-                    <button className="calculations">&#x2b;</button>
+                    <button className="numbers" value="1" onClick={this.props.one}>1</button>
+                    <button className="numbers" value="2" onClick={this.props.two}>2</button>
+                    <button className="numbers" value="3" onClick={this.props.three}>3</button>
+                    <button className="calculations" onClick={this.props.add}>&#x2b;</button>
                 </div>
                 {/* bottom */}
                 <div className="bottom buttonsFlex">
-                    <button className="numbers zero" value="0">0</button>
-                    <button className="numbers" value=".">.</button>
-                    <button className="calculations">&#x3d;</button>
+                    <button className="numbers zero" value="0" onClick={this.props.zero}>0</button>
+                    <button className="numbers" value="." onClick={this.props.dot}>.</button>
+                    <button className="calculations" equal={this.props.equal}>&#x3d;</button>
                 </div>
             </div>
         )
--- src/Calculator.js
+++ src/Calculator.js
@@ -7,15 +7,22 @@ class Calculator extends Component {
         super(props);
 
         this.state = {
-            display: 4
+            display: 0,
+            second: 0
+        }
+        if (this.state.display === 0) {
+            this.setState({
+                second: this.state.second = 3
+            });
         }
 
-        this.calculate = this.calculate.bind(this);
+        this.add = this.add.bind(this);
         this.clear = this.clear.bind(this);
+        this.concat = this.concat.bind(this);
     }
     // calcuate function
-    calculate(num1, num2) {
-        let firstNumber = num1,
+    add(num1, num2) {
+        let firstNumber = Number(num1),
             secondNumber = num2,
             total = firstNumber + secondNumber;
 
@@ -29,7 +36,20 @@ class Calculator extends Component {
             display: this.state.display = 0
         });
     }
+    concat(value) {
+        if (this.state.display !== 0) {
+            this.setState({
+                display: this.state.display += value
+            });
+        } else {
+            this.setState({
+                display: this.state.display = value
+            });
+        }
+    }
+    equal = () => {
 
+    }
     render() {
         const styles = {
             borderBottom: '2px solid gray',
@@ -37,12 +57,25 @@ class Calculator extends Component {
             padding: '10px',
             marginBottom: '50px'
         }
-
+        console.log(this.state.second);
         return (
             <div className="calculatorMain">
                 <h1 style={styles}>Calculator Made With React</h1>
                 <span className="display">{this.state.display}</span>
-                <Buttons clear={this.clear} />
+                <Buttons 
+                    clear={this.clear}
+                    dot={() => this.concat('.')}
+                    zero={() => this.concat('0')}
+                    one={() => this.concat('1')}
+                    two={() => this.concat('2')}
+                    three={() => this.concat('3')}
+                    four={() => this.concat('4')}
+                    five={() => this.concat('5')}
+                    six={() => this.concat('6')}
+                    seven={() => this.concat('7')}
+                    eight={() => this.concat('8')}
+                    nine={() => this.concat('9')} 
+                    add={() => this.add(this.state.display, this.state.second)}/>
             </div>
         );
     }
